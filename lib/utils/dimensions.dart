88913de4abbc357531c87c3d103163c3862839f6
Anshaj-logan <anshajahammed1110@gmail.com>
2023-06-14 20:13:40 +0530

    home-page UI

---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+
+class Dimensions {
+  static double screenHeight = Get.context!.height;
+  static double screenWidth = Get.context!.width;
+
+  static double pageView = screenHeight / 2.45;
+  static double pageViewContainer = screenHeight / 3.56;
+  static double pageViewTextContainer = screenHeight / 6.53;
+
+  static double height10 = screenHeight / 78.4;
+  static double height20 = screenHeight / 39.2;
+  static double height30 = screenHeight / 26.13;
+
+  static double width10 = screenHeight / 78.4;
+  static double width20 = screenHeight / 39.2;
+  static double width30 = screenHeight / 26.13;
+
+  static double font20 = screenHeight / 39.2;
+  static double radius20 = screenHeight / 39.2;
+  static double radius30 = screenHeight / 26.13;
+
+  //list view size
+
+  static double listViewImgSize = screenHeight / 6.53;
+  static double listViewTextContSize = screenHeight / 7.84;
+}
